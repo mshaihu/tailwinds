85bcc40946cdb3ebd4a19984c8b98f8d3756f618
Shaihu <mshaihu514@gmail.com>
2022-12-30 15:33:45 +0000

    innitial project

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,css}", "./src/**/*.{css,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
